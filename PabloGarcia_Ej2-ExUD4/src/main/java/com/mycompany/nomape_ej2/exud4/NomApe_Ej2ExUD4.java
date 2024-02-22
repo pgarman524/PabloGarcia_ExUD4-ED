f3426e394af6c7ae2f859b6d2179288a6abddbcf
pgarman524 <pgarman524@g.educaand.es>
2024-02-22 12:23:51 +0100

    Examen

---
+++ PabloGarcia_Ej2-ExUD4/src/main/java/com/mycompany/nomape_ej2/exud4/NomApe_Ej2ExUD4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nomape_ej2.exud4;
+
+/**
+ *
+ * @author Pgarman524
+ */
+public class NomApe_Ej2ExUD4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
